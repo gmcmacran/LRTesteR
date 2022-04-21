9cae27f4b8922591ea7f20d839cfa7868605f940
Greg McMahan <53842892+gmcmacran@users.noreply.github.com>
2022-04-20 20:29:04 -0500

    beta type 1 sim

--- R_scratch_code/MLTesteR_Type_1_Error_long_test.R
+++ R_scratch_code/MLTesteR_Type_1_Error_long_test.R
@@ -350,3 +350,122 @@ ggplot(temp, aes(x = factor(test), y = TypeI)) +
   geom_hline(yintercept = .05) +
   scale_y_continuous(breaks = seq(0, 1, .05), limits = c(0, 1)) +
   labs(x = "Hypothesis Test", y = "Type I Error")
+
+################
+# beta
+################
+
+B <- 1000
+N <- 200
+
+shape1s <- c(1:10, seq(15, 50, 10))
+shape2s <- c(1:10, seq(15, 50, 10))
+
+sim_results <- tibble()
+for (shape1 in shape1s) {
+  for (shape2 in shape2s) {
+    for (alt in c("two.sided", "less", "greater")) {
+      stats <- vector(mode = "numeric", length = B)
+      pvalues <- vector(mode = "numeric", length = B)
+      alts <- vector(mode = "character", length = B)
+      testName <- "beta_shape1_lr_test"
+      set.seed(1)
+      for (i in 1:B) {
+        x <- rbeta(N, shape1 = shape1, shape2 = shape2)
+        try(test <- beta_shape1_lr_test(x, shape1, alt))
+        try(stats[i] <- test$statistic)
+        try(pvalues[i] <- test$p.value)
+        try(alts[i] <- test$alternative)
+      }
+      temp <- tibble(test = testName, shape1 = shape1, shape2 = shape2, stat = stats, pvalue = pvalues, alt = alts)
+      sim_results <- sim_results %>% bind_rows(temp)
+      rm(stats, pvalues, alts, testName, temp, i)
+    }
+  }
+}
+sim_results %>% saveRDS("R_scratch_code/beta_type_one_shape1.rds")
+rm(sim_results)
+
+sim_results <- tibble()
+for (shape1 in shape1s) {
+  for (shape2 in shape2s) {
+    for (alt in c("two.sided", "less", "greater")) {
+      stats <- vector(mode = "numeric", length = B)
+      pvalues <- vector(mode = "numeric", length = B)
+      alts <- vector(mode = "character", length = B)
+      testName <- "beta_shape2_lr_test"
+      set.seed(1)
+      for (i in 1:B) {
+        x <- rbeta(N, shape1 = shape1, shape2 = shape2)
+        try(test <- beta_shape2_lr_test(x, shape2, alt))
+        try(stats[i] <- test$statistic)
+        try(pvalues[i] <- test$p.value)
+        try(alts[i] <- test$alternative)
+      }
+      temp <- tibble(test = testName, shape1 = shape1, shape2 = shape2, stat = stats, pvalue = pvalues, alt = alts)
+      sim_results <- sim_results %>% bind_rows(temp)
+      rm(stats, pvalues, alts, testName, temp, i)
+    }
+  }
+}
+sim_results %>% saveRDS("R_scratch_code/beta_type_one_shape2.rds")
+rm(sim_results)
+
+sim_results <- bind_rows(
+  readRDS(file = "R_scratch_code/beta_type_one_shape1.rds"),
+  readRDS(file = "R_scratch_code/beta_type_one_shape2.rds")
+)
+
+# Check structure
+sim_results %>%
+  distinct(test) %>%
+  nrow() == 2
+
+sim_results %>%
+  distinct(shape1) %>%
+  nrow() == length(shape1s)
+
+sim_results %>%
+  distinct(shape2) %>%
+  nrow() == length(shape2s)
+
+sim_results %>%
+  distinct(alt) %>%
+  nrow() == 3
+
+sim_results %>%
+  pull(pvalue) %>%
+  min(na.rm = TRUE) >= 0
+
+sim_results %>%
+  pull(pvalue) %>%
+  max(na.rm = TRUE) <= 1
+
+temp <- sim_results %>%
+  group_by(test, alt, shape1, shape2) %>%
+  summarise(TypeI = mean(pvalue <= .05, na.rm = TRUE), meanStat = mean(stat, na.rm = TRUE), N = sum(!is.na(pvalue))) %>%
+  arrange(desc(TypeI))
+
+ggplot(temp, aes(x = factor(shape1), y = TypeI)) +
+  geom_boxplot() +
+  geom_hline(yintercept = .05) +
+  scale_y_continuous(breaks = seq(0, 1, .05), limits = c(0, 1)) +
+  labs(x = "Shape 1", y = "Type I Error")
+
+ggplot(temp, aes(x = factor(shape2), y = TypeI)) +
+  geom_boxplot() +
+  geom_hline(yintercept = .05) +
+  scale_y_continuous(breaks = seq(0, 1, .05), limits = c(0, 1)) +
+  labs(x = "Shape 2", y = "Type I Error")
+
+ggplot(temp, aes(x = factor(alt), y = TypeI)) +
+  geom_boxplot() +
+  geom_hline(yintercept = .05) +
+  scale_y_continuous(breaks = seq(0, 1, .05), limits = c(0, 1)) +
+  labs(x = "Alternative Hypothesis", y = "Type I Error")
+
+ggplot(temp, aes(x = factor(test), y = TypeI)) +
+  geom_boxplot() +
+  geom_hline(yintercept = .05) +
+  scale_y_continuous(breaks = seq(0, 1, .05), limits = c(0, 1)) +
+  labs(x = "Hypothesis Test", y = "Type I Error")
