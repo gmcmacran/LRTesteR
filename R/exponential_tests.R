d9a01b892de461467b6171bd7434c6772d9f59b6
Greg McMahan <53842892+gmcmacran@users.noreply.github.com>
2022-08-09 19:32:59 -0500

    add equality of rates test

--- R/exponential_tests.R
+++ R/exponential_tests.R
@@ -1,5 +1,5 @@
 #' @keywords internal
-calc_test_stat_exponentail_rate <- function(x, rate, alternative) {
+calc_test_stat_exponential_rate <- function(x, rate, alternative) {
   obs_rate <- 1 / mean(x)
 
   W <- 2 * (sum(stats::dexp(x = x, rate = obs_rate, log = TRUE)) -
@@ -24,11 +24,65 @@ calc_test_stat_exponentail_rate <- function(x, rate, alternative) {
 #' # Null is true
 #' set.seed(1)
 #' x <- rexp(100, 1)
-#' exponentail_rate_lr_test(x, 1, "two.sided")
+#' exponential_rate_one_sample(x, 1, "two.sided")
 #'
 #' # Null is false
 #' set.seed(1)
 #' x <- rexp(100, 3)
-#' exponentail_rate_lr_test(x, 1, "greater")
+#' exponential_rate_one_sample(x, 1, "greater")
 #' @export
-exponentail_rate_lr_test <- LRTesteR:::create_test_function_continuous(LRTesteR:::calc_test_stat_exponentail_rate, rate, 0)
+exponential_rate_one_sample <- LRTesteR:::create_test_function_continuous(LRTesteR:::calc_test_stat_exponential_rate, rate, 0)
+
+#' @keywords internal
+calc_test_stat_exponential_rate_one_way <- function(x, fctr) {
+  # Null
+  obs_lambda <- 1 / base::mean(x)
+
+  W1 <- sum(stats::dexp(x = x, rate = obs_lambda, log = TRUE))
+
+  # alt
+  group_lambdas <- vector(mode = "numeric", length = length(levels(fctr)))
+  likelihoods <- vector(mode = "numeric", length = length(levels(fctr)))
+  for (i in 1:length(levels(fctr))) {
+    l <- levels(fctr)[i]
+    index <- which(fctr == l)
+    tempX <- x[index]
+    temp <- 1 / base::mean(tempX)
+    group_lambdas[i] <- temp
+    temp <- sum(stats::dexp(x = tempX, rate = group_lambdas[i], log = TRUE))
+    likelihoods[i] <- temp
+  }
+
+  W2 <- sum(likelihoods)
+
+  W <- 2 * (W2 - W1)
+
+  return(W)
+}
+
+#' Test equality of lambda of poisson distributions.
+#'
+#' @inheritParams gaussian_mu_one_way
+#' @inherit gaussian_mu_one_way return
+#' @inherit gaussian_mu_one_way source
+#' @details
+#' Null: All lambdas are equal. (lambda_1 = lambda_2 ... lambda_k).
+#' Alternative: At least one lambda is not equal.
+#' @examples
+#' library(LRTesteR)
+#'
+#' # Null is true
+#' set.seed(1)
+#' x <- rexp(150, 1)
+#' fctr <- c(rep(1, 50), rep(2, 50), rep(3, 50))
+#' fctr <- factor(fctr, levels = c("1", "2", "3"))
+#' exponential_rate_one_way(x, fctr, .95)
+#'
+#' # Null is false
+#' set.seed(1)
+#' x <- c(rexp(50, 1), rexp(50, 2), rexp(50, 3))
+#' fctr <- c(rep(1, 50), rep(2, 50), rep(3, 50))
+#' fctr <- factor(fctr, levels = c("1", "2", "3"))
+#' exponential_rate_one_way(x, fctr, .95)
+#' @export
+exponential_rate_one_way <- create_test_function_continuous_one_way(LRTesteR:::calc_test_stat_exponential_rate_one_way, exponential_rate_one_sample)
--- tests/testthat/test_exponential_tests.R
+++ tests/testthat/test_exponential_tests.R
@@ -34,7 +34,7 @@ exact_test <- function(x, rate, alternative) {
 for (alt in c("two.sided", "greater", "less")) {
   set.seed(1)
   x <- rexp(200, 1)
-  test <- exponentail_rate_lr_test(x, 1, alt)
+  test <- exponential_rate_one_sample(x, 1, alt)
 
   test_that("Check structure.", {
     expect_true(class(test) == "lrtest")
@@ -52,8 +52,8 @@ for (alt in c("two.sided", "greater", "less")) {
   CI1 <- test$conf.int[1] + .Machine$double.eps # Avoid boundary
   CI2 <- test$conf.int[2] - .Machine$double.eps
   test_that("Check CI", {
-    expect_true(ifelse(is.finite(CI1), exponentail_rate_lr_test(x, CI1, alt)$p.value, .05) >= .0499)
-    expect_true(ifelse(is.finite(CI2), exponentail_rate_lr_test(x, CI2, alt)$p.value, .05) >= .0499)
+    expect_true(ifelse(is.finite(CI1), exponential_rate_one_sample(x, CI1, alt)$p.value, .05) >= .0499)
+    expect_true(ifelse(is.finite(CI2), exponential_rate_one_sample(x, CI2, alt)$p.value, .05) >= .0499)
   })
   rm(CI1, CI2)
 }
@@ -64,7 +64,7 @@ for (alt in c("two.sided", "greater", "less")) {
 for (alt in c("two.sided", "greater")) {
   set.seed(1)
   x <- rexp(200, 3)
-  test <- exponentail_rate_lr_test(x, 1, alt)
+  test <- exponential_rate_one_sample(x, 1, alt)
 
   test_that("Check structure.", {
     expect_true(class(test) == "lrtest")
@@ -81,8 +81,8 @@ for (alt in c("two.sided", "greater")) {
   CI1 <- test$conf.int[1] + .Machine$double.eps # Avoid boundary
   CI2 <- test$conf.int[2] - .Machine$double.eps
   pval <- pmin(
-    ifelse(is.finite(CI1), exponentail_rate_lr_test(x, CI1, alt)$p.value, .05),
-    ifelse(is.finite(CI2), exponentail_rate_lr_test(x, CI2, alt)$p.value, .05)
+    ifelse(is.finite(CI1), exponential_rate_one_sample(x, CI1, alt)$p.value, .05),
+    ifelse(is.finite(CI2), exponential_rate_one_sample(x, CI2, alt)$p.value, .05)
   )
   test_that("Check CI", {
     expect_true(pval <= .0500001)
@@ -93,7 +93,7 @@ for (alt in c("two.sided", "greater")) {
 for (alt in c("two.sided", "less")) {
   set.seed(1)
   x <- rexp(200, 1)
-  test <- exponentail_rate_lr_test(x, 3, alt)
+  test <- exponential_rate_one_sample(x, 3, alt)
 
   test_that("Check structure.", {
     expect_true(class(test) == "lrtest")
@@ -110,8 +110,8 @@ for (alt in c("two.sided", "less")) {
   CI1 <- test$conf.int[1] + .Machine$double.eps # Avoid boundary
   CI2 <- test$conf.int[2] - .Machine$double.eps
   pval <- pmin(
-    ifelse(is.finite(CI1), exponentail_rate_lr_test(x, CI1, alt)$p.value, .05),
-    ifelse(is.finite(CI2), exponentail_rate_lr_test(x, CI2, alt)$p.value, .05)
+    ifelse(is.finite(CI1), exponential_rate_one_sample(x, CI1, alt)$p.value, .05),
+    ifelse(is.finite(CI2), exponential_rate_one_sample(x, CI2, alt)$p.value, .05)
   )
   test_that("Check CI", {
     expect_true(pval <= .0500001)
@@ -123,39 +123,39 @@ for (alt in c("two.sided", "less")) {
 # Input checking
 ###############################################
 test_that("x input checking works", {
-  expect_error(exponentail_rate_lr_test(c()), "Argument x should have at least 50 data points.")
-  expect_error(exponentail_rate_lr_test(rep("foo", 50)), "Argument x should be numeric.")
+  expect_error(exponential_rate_one_sample(c()), "Argument x should have at least 50 data points.")
+  expect_error(exponential_rate_one_sample(rep("foo", 50)), "Argument x should be numeric.")
 })
 
 set.seed(1)
 x <- rexp(50)
 test_that("rate input checking works", {
-  expect_error(exponentail_rate_lr_test(x, c(1, 2)), "The tested parameter should have length one.")
-  expect_error(exponentail_rate_lr_test(x, "foo"), "The tested parameter should be numeric.")
-  expect_error(exponentail_rate_lr_test(x, 0), "The tested parameter should be above 0.")
+  expect_error(exponential_rate_one_sample(x, c(1, 2)), "The tested parameter should have length one.")
+  expect_error(exponential_rate_one_sample(x, "foo"), "The tested parameter should be numeric.")
+  expect_error(exponential_rate_one_sample(x, 0), "The tested parameter should be above 0.")
 })
 
 test_that("alternative input checking works", {
-  expect_error(exponentail_rate_lr_test(x, 1, c("two.sided", "less")), "Argument alternative should have length one.")
-  expect_error(exponentail_rate_lr_test(x, 1, 1), "Argument alternative should be a character.")
-  expect_error(exponentail_rate_lr_test(x, 1, "lesss"), "Argument alternative should be 'two.sided', 'less', or 'greater.")
+  expect_error(exponential_rate_one_sample(x, 1, c("two.sided", "less")), "Argument alternative should have length one.")
+  expect_error(exponential_rate_one_sample(x, 1, 1), "Argument alternative should be a character.")
+  expect_error(exponential_rate_one_sample(x, 1, "lesss"), "Argument alternative should be 'two.sided', 'less', or 'greater.")
 })
 
 test_that("conf.level input checking works", {
-  expect_error(exponentail_rate_lr_test(x, 1, "less", c(.50, .75)), "conf.level should have length one.")
-  expect_error(exponentail_rate_lr_test(x, 1, "less", "foo"), "conf.level should be numeric.")
-  expect_error(exponentail_rate_lr_test(x, 1, "less", 0), "conf.level should between zero and one.")
-  expect_error(exponentail_rate_lr_test(x, 1, "less", 1), "conf.level should between zero and one.")
+  expect_error(exponential_rate_one_sample(x, 1, "less", c(.50, .75)), "conf.level should have length one.")
+  expect_error(exponential_rate_one_sample(x, 1, "less", "foo"), "conf.level should be numeric.")
+  expect_error(exponential_rate_one_sample(x, 1, "less", 0), "conf.level should between zero and one.")
+  expect_error(exponential_rate_one_sample(x, 1, "less", 1), "conf.level should between zero and one.")
 })
 
 ###############################################
 # Null True
 ###############################################
 set.seed(1)
-x <- rpois(150, 1)
+x <- rexp(150, 1)
 fctr <- c(rep(1, 50), rep(2, 50), rep(3, 50))
 fctr <- factor(fctr, levels = c("1", "2", "3"))
-test <- poisson_lambda_one_way(x, fctr, .95)
+test <- exponential_rate_one_way(x, fctr, .95)
 
 test_that("Check structure.", {
   expect_true(class(test) == "lrtest")
@@ -163,33 +163,27 @@ test_that("Check structure.", {
   expect_true(all(names(test) == c("statistic", "p.value", "conf.ints", "overall.conf", "individ.conf", "alternative")))
 })
 
-dat <- data.frame(fctr = fctr, x = x)
-model_00 <- glm(x ~ 1, data = dat, family = poisson(link = "identity"))
-model_01 <- glm(x ~ fctr, data = dat, family = poisson(link = "identity"))
-
-test_02 <- lmtest::lrtest(model_00, model_01)
 test_that("Check contents", {
   expect_true(test$p.value > .05)
-  expect_equal(test$p.value, test_02[["Pr(>Chisq)"]][2])
 })
 
 # make sure CIs match
 CI1 <- unname(test$conf.ints[[1]])
-CI2 <- poisson_lambda_lr_test(x[which(fctr == 1)], 2, test$alternative, test$individ.conf)$conf.int
+CI2 <- exponential_rate_one_sample(x[which(fctr == 1)], 2, test$alternative, test$individ.conf)$conf.int
 test_that("Check CI", {
   expect_equal(CI1, CI2)
 })
-rm(CI1, CI2, dat, model_00, model_01)
+rm(CI1, CI2)
 
 ###############################################
 # Null False
 ###############################################
 
 set.seed(1)
-x <- c(rpois(50, 1), rpois(50, 1.50), rpois(50, 2))
+x <- c(rexp(50, 1), rexp(50, 2), rexp(50, 3))
 fctr <- c(rep(1, 50), rep(2, 50), rep(3, 50))
 fctr <- factor(fctr, levels = c("1", "2", "3"))
-test <- poisson_lambda_one_way(x, fctr, .95)
+test <- exponential_rate_one_way(x, fctr, .95)
 
 test_that("Check structure.", {
   expect_true(class(test) == "lrtest")
@@ -197,50 +191,44 @@ test_that("Check structure.", {
   expect_true(all(names(test) == c("statistic", "p.value", "conf.ints", "overall.conf", "individ.conf", "alternative")))
 })
 
-dat <- data.frame(fctr = fctr, x = x)
-model_00 <- glm(x ~ 1, data = dat, family = poisson(link = "identity"))
-model_01 <- glm(x ~ fctr, data = dat, family = poisson(link = "identity"))
-
-test_02 <- lmtest::lrtest(model_00, model_01)
 test_that("Check contents", {
   expect_true(test$p.value < .05)
-  expect_equal(test$p.value, test_02[["Pr(>Chisq)"]][2])
 })
 
 # make sure CIs match
 CI1 <- unname(test$conf.ints[[1]])
-CI2 <- poisson_lambda_lr_test(x[which(fctr == 1)], 2, test$alternative, test$individ.conf)$conf.int
+CI2 <- exponential_rate_one_sample(x[which(fctr == 1)], 2, test$alternative, test$individ.conf)$conf.int
 test_that("Check CI", {
   expect_equal(CI1, CI2)
 })
-rm(CI1, CI2, dat, model_00, model_01)
+rm(CI1, CI2)
 
 ###############################################
 # Input checking
 ###############################################
 test_that("x input checking works", {
-  expect_error(poisson_lambda_one_way(c()), "Argument x should have at least 50 data points.")
-  expect_error(poisson_lambda_one_way(rep("foo", 50)), "Argument x should be numeric.")
+  expect_error(exponential_rate_one_way(c()), "Argument x should have at least 50 data points.")
+  expect_error(exponential_rate_one_way(rep("foo", 50)), "Argument x should be numeric.")
 })
 
 set.seed(1)
-x <- rnorm(100)
+x <- rpois(100, 1)
 fctr1 <- factor(rep(1, 100), levels = c("1", "2", "3"))
 fctr2 <- factor(c(rep(1, 60), rep(2, 40)), levels = c("1", "2", "3"))
 test_that("fctr input checking works", {
-  expect_error(poisson_lambda_one_way(x, "foo"), "Argument fctr should have same length as x.")
-  expect_error(poisson_lambda_one_way(x, rep("foo", 100)), "Argument fctr should be a factor.")
-  expect_error(poisson_lambda_one_way(x, factor(rep("foo", 100))), "Argument fctr should have at least two unique values.")
-  expect_error(poisson_lambda_one_way(x, fctr1), "Argument fctr should have at least two unique values.")
-  expect_error(poisson_lambda_one_way(x, fctr2), "Each groups needs to contain at least 50 points for CIs to be accurate.")
+  expect_error(exponential_rate_one_way(x, "foo"), "Argument fctr should have same length as x.")
+  expect_error(exponential_rate_one_way(x, rep("foo", 100)), "Argument fctr should be a factor.")
+  expect_error(exponential_rate_one_way(x, factor(rep("foo", 100))), "Argument fctr should have at least two unique values.")
+  expect_error(exponential_rate_one_way(x, fctr1), "Argument fctr should have at least two unique values.")
+  expect_error(exponential_rate_one_way(x, fctr2), "Each groups needs to contain at least 50 points for CIs to be accurate.")
 })
 rm(fctr1, fctr2)
 
 fctr <- c(rep(1, 50), rep(2, 50))
 fctr <- factor(fctr, levels = c("1", "2"))
 test_that("conf.level input checking works", {
-  expect_error(poisson_lambda_one_way(x, fctr, c(.50, .75)), "conf.level should have length one.")
-  expect_error(poisson_lambda_one_way(x, fctr, "foo"), "conf.level should be numeric.")
-  expect_error(poisson_lambda_one_way(x, fctr, 0), "conf.level should between zero and one.")
-  expect_error(poisson_lambda_one_way(x, fctr, 1), "conf.level should between zero and one.")
+  expect_error(exponential_rate_one_way(x, fctr, c(.50, .75)), "conf.level should have length one.")
+  expect_error(exponential_rate_one_way(x, fctr, "foo"), "conf.level should be numeric.")
+  expect_error(exponential_rate_one_way(x, fctr, 0), "conf.level should between zero and one.")
+  expect_error(exponential_rate_one_way(x, fctr, 1), "conf.level should between zero and one.")
 })
