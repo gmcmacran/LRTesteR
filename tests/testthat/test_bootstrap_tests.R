f091bc5ecb911f98aaa1941964648e2916693de5
Greg McMahan <53842892+gmcmacran@users.noreply.github.com>
2023-03-18 10:56:11 -0500

    param.fun to stat.fun

--- tests/testthat/test_bootstrap_tests.R
+++ tests/testthat/test_bootstrap_tests.R
@@ -1,14 +1,14 @@
 ###############################################
 # Null True (mean test)
 ###############################################
-param.fun <- function(x) {
+stat.fun <- function(x) {
   return(mean(x))
 }
 
 for (alt in c("two.sided", "greater", "less")) {
   set.seed(1)
   x <- rnorm(200, 0, 1)
-  test <- bootstrap_one_sample(x, 0, param.fun, alt)
+  test <- bootstrap_one_sample(x, 0, stat.fun, alt)
 
   test_that("Check structure.", {
     expect_true(all(class(test) == c("one_sample_case_four", "lrtest")))
@@ -33,8 +33,8 @@ for (alt in c("two.sided", "greater", "less")) {
   CI1 <- test$conf.int[1] + .Machine$double.eps # Avoid boundary
   CI2 <- test$conf.int[2] - .Machine$double.eps
   test_that("Check CI", {
-    expect_true(ifelse(is.finite(CI1), bootstrap_one_sample(x, CI1, param.fun, alt)$p.value, .05) >= .0498)
-    expect_true(ifelse(is.finite(CI2), bootstrap_one_sample(x, CI2, param.fun, alt)$p.value, .05) >= .0498)
+    expect_true(ifelse(is.finite(CI1), bootstrap_one_sample(x, CI1, stat.fun, alt)$p.value, .05) >= .0498)
+    expect_true(ifelse(is.finite(CI2), bootstrap_one_sample(x, CI2, stat.fun, alt)$p.value, .05) >= .0498)
   })
   rm(CI1, CI2)
 }
@@ -45,7 +45,7 @@ for (alt in c("two.sided", "greater", "less")) {
 for (alt in c("two.sided", "greater")) {
   set.seed(1)
   x <- rnorm(200, 3, 1)
-  test <- bootstrap_one_sample(x, 0, param.fun, alt)
+  test <- bootstrap_one_sample(x, 0, stat.fun, alt)
 
   test_that("Check structure.", {
     expect_true(all(class(test) == c("one_sample_case_four", "lrtest")))
@@ -69,8 +69,8 @@ for (alt in c("two.sided", "greater")) {
   CI1 <- test$conf.int[1] + .Machine$double.eps # Avoid boundary
   CI2 <- test$conf.int[2] - .Machine$double.eps
   pval <- pmin(
-    ifelse(is.finite(CI1), bootstrap_one_sample(x, CI1, param.fun, alt)$p.value, .05),
-    ifelse(is.finite(CI2), bootstrap_one_sample(x, CI2, param.fun, alt)$p.value, .05)
+    ifelse(is.finite(CI1), bootstrap_one_sample(x, CI1, stat.fun, alt)$p.value, .05),
+    ifelse(is.finite(CI2), bootstrap_one_sample(x, CI2, stat.fun, alt)$p.value, .05)
   )
   test_that("Check CI", {
     expect_true(pval <= .0500001)
@@ -81,7 +81,7 @@ for (alt in c("two.sided", "greater")) {
 for (alt in c("two.sided", "less")) {
   set.seed(1)
   x <- rnorm(200, -3, 1)
-  test <- bootstrap_one_sample(x, -2, param.fun, alt)
+  test <- bootstrap_one_sample(x, -2, stat.fun, alt)
 
   test_that("Check structure.", {
     expect_true(all(class(test) == c("one_sample_case_four", "lrtest")))
@@ -105,8 +105,8 @@ for (alt in c("two.sided", "less")) {
   CI1 <- test$conf.int[1] + .Machine$double.eps # Avoid boundary
   CI2 <- test$conf.int[2] - .Machine$double.eps
   pval <- pmin(
-    ifelse(is.finite(CI1), bootstrap_one_sample(x, CI1, param.fun, alt)$p.value, .05),
-    ifelse(is.finite(CI2), bootstrap_one_sample(x, CI2, param.fun, alt)$p.value, .05)
+    ifelse(is.finite(CI1), bootstrap_one_sample(x, CI1, stat.fun, alt)$p.value, .05),
+    ifelse(is.finite(CI2), bootstrap_one_sample(x, CI2, stat.fun, alt)$p.value, .05)
   )
   test_that("Check CI", {
     expect_true(pval <= .0500001)
@@ -117,14 +117,14 @@ for (alt in c("two.sided", "less")) {
 ###############################################
 # Null True (variance test)
 ###############################################
-param.fun <- function(x) {
+stat.fun <- function(x) {
   return(sum((x - mean(x))^2) / length(x))
 }
 
 for (alt in c("two.sided", "greater", "less")) {
   set.seed(1)
   x <- rnorm(200, 0, 3)
-  test <- bootstrap_one_sample(x, 9, param.fun, alt)
+  test <- bootstrap_one_sample(x, 9, stat.fun, alt)
 
   test_that("Check structure.", {
     expect_true(all(class(test) == c("one_sample_case_four", "lrtest")))
@@ -149,8 +149,8 @@ for (alt in c("two.sided", "greater", "less")) {
   CI1 <- test$conf.int[1] + .Machine$double.eps # Avoid boundary
   CI2 <- test$conf.int[2] - .Machine$double.eps
   test_that("Check CI", {
-    expect_true(ifelse(is.finite(CI1), bootstrap_one_sample(x, CI1, param.fun, alt)$p.value, .05) >= .045)
-    expect_true(ifelse(is.finite(CI2), bootstrap_one_sample(x, CI2, param.fun, alt)$p.value, .05) >= .045)
+    expect_true(ifelse(is.finite(CI1), bootstrap_one_sample(x, CI1, stat.fun, alt)$p.value, .05) >= .045)
+    expect_true(ifelse(is.finite(CI2), bootstrap_one_sample(x, CI2, stat.fun, alt)$p.value, .05) >= .045)
   })
   rm(CI1, CI2)
 }
@@ -161,7 +161,7 @@ for (alt in c("two.sided", "greater", "less")) {
 for (alt in c("two.sided", "greater")) {
   set.seed(2)
   x <- rnorm(200, 0, 3)
-  test <- bootstrap_one_sample(x, 8, param.fun, alt)
+  test <- bootstrap_one_sample(x, 8, stat.fun, alt)
 
   test_that("Check structure.", {
     expect_true(all(class(test) == c("one_sample_case_four", "lrtest")))
@@ -185,8 +185,8 @@ for (alt in c("two.sided", "greater")) {
   CI1 <- test$conf.int[1] + .Machine$double.eps # Avoid boundary
   CI2 <- test$conf.int[2] - .Machine$double.eps
   pval <- pmin(
-    ifelse(is.finite(CI1), bootstrap_one_sample(x, CI1, param.fun, alt)$p.value, .05),
-    ifelse(is.finite(CI2), bootstrap_one_sample(x, CI2, param.fun, alt)$p.value, .05)
+    ifelse(is.finite(CI1), bootstrap_one_sample(x, CI1, stat.fun, alt)$p.value, .05),
+    ifelse(is.finite(CI2), bootstrap_one_sample(x, CI2, stat.fun, alt)$p.value, .05)
   )
   test_that("Check CI", {
     expect_true(pval <= .0500001)
@@ -197,7 +197,7 @@ for (alt in c("two.sided", "greater")) {
 for (alt in c("two.sided", "less")) {
   set.seed(1)
   x <- rnorm(200, 0, 3)
-  test <- bootstrap_one_sample(x, 10, param.fun, alt)
+  test <- bootstrap_one_sample(x, 10, stat.fun, alt)
 
   test_that("Check structure.", {
     expect_true(all(class(test) == c("one_sample_case_four", "lrtest")))
@@ -221,8 +221,8 @@ for (alt in c("two.sided", "less")) {
   CI1 <- test$conf.int[1] + .Machine$double.eps # Avoid boundary
   CI2 <- test$conf.int[2] - .Machine$double.eps
   pval <- pmin(
-    ifelse(is.finite(CI1), bootstrap_one_sample(x, CI1, param.fun, alt)$p.value, .05),
-    ifelse(is.finite(CI2), bootstrap_one_sample(x, CI2, param.fun, alt)$p.value, .05)
+    ifelse(is.finite(CI1), bootstrap_one_sample(x, CI1, stat.fun, alt)$p.value, .05),
+    ifelse(is.finite(CI2), bootstrap_one_sample(x, CI2, stat.fun, alt)$p.value, .05)
   )
   test_that("Check CI", {
     expect_true(pval <= .0500001)
@@ -247,20 +247,20 @@ test_that("mu input checking works", {
 helper_00 <- function(bar) {}
 helper_01 <- function(x, bar) {}
 test_that("mu input checking works", {
-  expect_error(bootstrap_one_sample(x, 0, "foo"), "Argument param.fun must be a function.")
-  expect_error(bootstrap_one_sample(x, 0, helper_00), "param.fun's first argument is not x.")
-  expect_error(bootstrap_one_sample(x, 0, helper_01), "param.fun has too many arguments.")
+  expect_error(bootstrap_one_sample(x, 0, "foo"), "Argument stat.fun must be a function.")
+  expect_error(bootstrap_one_sample(x, 0, helper_00), "stat.fun's first argument is not x.")
+  expect_error(bootstrap_one_sample(x, 0, helper_01), "stat.fun has too many arguments.")
 })
 
 test_that("alternative input checking works", {
-  expect_error(bootstrap_one_sample(x, 0, param.fun, c("two.sided", "less")), "Argument alternative should have length one.")
-  expect_error(bootstrap_one_sample(x, 0, param.fun, 1), "Argument alternative should be a character.")
-  expect_error(bootstrap_one_sample(x, 0, param.fun, "lesss"), "Argument alternative should be 'two.sided', 'less', or 'greater.")
+  expect_error(bootstrap_one_sample(x, 0, stat.fun, c("two.sided", "less")), "Argument alternative should have length one.")
+  expect_error(bootstrap_one_sample(x, 0, stat.fun, 1), "Argument alternative should be a character.")
+  expect_error(bootstrap_one_sample(x, 0, stat.fun, "lesss"), "Argument alternative should be 'two.sided', 'less', or 'greater.")
 })
 
 test_that("conf.level input checking works", {
-  expect_error(bootstrap_one_sample(x, 1, param.fun, "less", c(.50, .75)), "conf.level should have length one.")
-  expect_error(bootstrap_one_sample(x, 1, param.fun, "less", "foo"), "conf.level should be numeric.")
-  expect_error(bootstrap_one_sample(x, 1, param.fun, "less", 0), "conf.level should between zero and one.")
-  expect_error(bootstrap_one_sample(x, 1, param.fun, "less", 1), "conf.level should between zero and one.")
+  expect_error(bootstrap_one_sample(x, 1, stat.fun, "less", c(.50, .75)), "conf.level should have length one.")
+  expect_error(bootstrap_one_sample(x, 1, stat.fun, "less", "foo"), "conf.level should be numeric.")
+  expect_error(bootstrap_one_sample(x, 1, stat.fun, "less", 0), "conf.level should between zero and one.")
+  expect_error(bootstrap_one_sample(x, 1, stat.fun, "less", 1), "conf.level should between zero and one.")
 })
