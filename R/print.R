0d21adc515d33e633a048562393965f29326ec5c
Greg McMahan <53842892+gmcmacran@users.noreply.github.com>
2023-09-24 09:26:27 -0500

    print to cat

--- R/print.R
+++ R/print.R
@@ -18,17 +18,29 @@
 #' print(test)
 #' @export
 print.lrtest <- function(x, ...) {
-  print(paste("Log Likelihood Statistic:", round(x$statistic, 2), sep = " "))
-  print(paste("p value:", round(x$p.value, 3), sep = " "))
+  cat(paste("Log Likelihood Statistic:", round(x$statistic, 2), sep = " "))
+  cat("\n")
+
+  cat(paste("p value:", round(x$p.value, 3), sep = " "))
+  cat("\n")
   if (class(x)[1] %in% c("one_sample_case_one", "one_sample_case_two", "one_sample_case_three", "one_sample_case_four")) {
-    print(paste("Confidence Level: ", round(x[["conf.level"]][1], 3) * 100, "%", sep = ""))
-    print(paste("Confidence Interval: (", round(x[["conf.int"]][1], 3), ", ", round(x[["conf.int"]][2], 3), ")", sep = ""))
+    cat(paste("Confidence Level: ", round(x[["conf.level"]][1], 3) * 100, "%", sep = ""))
+    cat("\n")
+
+    cat(paste("Confidence Interval: (", round(x[["conf.int"]][1], 3), ", ", round(x[["conf.int"]][2], 3), ")", sep = ""))
+    cat("\n")
   } else {
     CIs <- x$conf.ints
-    print(paste("Confidence Level Of Set: ", round(x[["overall.conf"]][1], 3) * 100, "%", sep = ""))
-    print(paste("Individual Confidence Level: ", round(x[["individ.conf"]][1], 3) * 100, "%", sep = ""))
+    cat(paste("Confidence Level Of Set: ", round(x[["overall.conf"]][1], 3) * 100, "%", sep = ""))
+    cat("\n")
+
+    cat(paste("Individual Confidence Level: ", round(x[["individ.conf"]][1], 3) * 100, "%", sep = ""))
+    cat("\n")
     for (i in seq(1, length(CIs), 1)) {
-      print(paste("Confidence Interval For Group ", names(CIs)[i], ": (", round(CIs[[i]][1], 3), ", ", round(CIs[[i]][2], 3), ")", sep = ""))
+      cat(paste("Confidence Interval For Group ", names(CIs)[i], ": (", round(CIs[[i]][1], 3), ", ", round(CIs[[i]][2], 3), ")", sep = ""))
+      cat("\n")
     }
   }
+
+  invisible(x)
 }
